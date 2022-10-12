0be9792b501ef87d3ab1a692155a72e81631a962
Irina-Bondarenko <96590953+Irina-Bondarenko@users.noreply.github.com>
2022-10-12 23:53:06 +0300

    init

---
+++ card/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Card from "./components/Card";
+
+function App() {
+  return (
+    <div className="App">
+
+      <Card>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Body>Card Body</Card.Body>
+      </Card>
+
+    </div>
+  );
+}
+
+export default App;
